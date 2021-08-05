2a674a0468946d466c4e8f7dfdeab38043ff3b84
Saro Amirkhanyan <saroamirkhanyan@gmail.com>
2021-08-05 09:02:22 +0400

    created function for controlling function queue

--- application/domain/kodikdb/updateVideos.js
+++ application/domain/kodikdb/updateVideos.js
@@ -1,35 +1,29 @@
 ({ url, type }) => new Promise(async (resolve, reject) => {
-  const searialTypeId = await db.pg.col('VideoType', 'videoTypeId', { name: type }).then(rows => rows[0])
+  const videoTypeId = await db.pg.col('VideoType', 'videoTypeId', { name: type }).then(rows => rows[0])
   const parser = npm.JSONStream.parse('*')
-
+  const videoQueue = lib.utils.createQueue()
   let isFinished = false;
-  let unfinished = 0;
-
-  const finishIfEnd = () => {
-    if(isFinished && unfinished < 1) {
-      resolve()
-    }
-  }
-
-  parser.on('data', async video => {
-    unfinished++;
-    const rows = await db.pg.col('Video', 'videoId', { stringId: video.id })
-    if(rows.length)  {
-      return unfinished-- && finishIfEnd();
-    };
-    await db.pg.insert("Video", {
-      stringId: video.id,
-      videoTypeId: searialTypeId,
-      title: video.title,
-      title_original: video.title_orig,
-      description: video.material_data?.description || "",
-      host: "Kodik",
-      source: video.player_link,
-      thumbnail: video.material_data?.poster_url || '/images/not-thumbnail.png',
+  videoQueue.setIntermediateTime(500)
+  videoQueue.onfinish(() => {
+    if(isFinished) resolve()
+    else reject("Not finished!")
+  })
+  parser.on('data', video => {
+    videoQueue.add(async () => {
+      const rows = await db.pg.col('Video', 'videoId', { stringId: video.id })
+      if(rows.length) return;
+      await db.pg.insert("Video", {
+        stringId: video.id,
+        videoTypeId,
+        title: video.title,
+        title_original: video.title_orig,
+        description: video.material_data?.description || "",
+        host: "Kodik",
+        source: video.player_link,
+        thumbnail: video.material_data?.poster_url || '/images/not-thumbnail.png',
+      })
+      await db.pg.query(`UPDATE "Video" video SET "videoTokens" = to_tsvector(video.title || ' ' || video.title_original) WHERE "stringId" = $1`, [video.id])
     })
-    await db.pg.query(`UPDATE "Video" video SET "videoTokens" = to_tsvector(video.title || ' ' || video.title_original) WHERE "stringId" = $1`, [video.id])
-    unfinished--;
-    finishIfEnd()
   })
 
   parser.on('end', () => isFinished = true)
