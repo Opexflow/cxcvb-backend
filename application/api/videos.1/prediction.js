573cbb182dc8f2c068b193e7e6dcdfa2f6b546d6
Saro Amirkhanyan <saroamirkhanyan@gmail.com>
2021-08-11 20:13:51 +0400

    added limit to prediction

--- application/api/videos.1/prediction.js
+++ application/api/videos.1/prediction.js
@@ -3,7 +3,7 @@
   method: async ({ query }) => {
     if(typeof query !== "string") throw new Error("query parameter must be a string")
     return db.pg
-      .query(`SELECT DISTINCT "title" FROM "Video" WHERE title LIKE $1`, [`%${query}%`])
+      .query(`SELECT DISTINCT "title" FROM "Video" WHERE title LIKE $1 LIMIT 10`, [`%${query}%`])
       .then(result => result.rows)
   }
 })
