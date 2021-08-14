529c9e0793e8b718bee823814aa41372652a85c6
Saro Amirkhanyan <saroamirkhanyan@gmail.com>
2021-08-14 20:09:17 +0400

    Removed scrapping part from videos/search to domain/scrapnet, added seperateTime function

--- application/api/videos.1/search.js
+++ application/api/videos.1/search.js
@@ -9,84 +9,6 @@
     if (!validation.valid) return new Error(validation.errors)
     if (count > 30) return new Error("Maximum count is 30")
     if (count < 1) return new Error("Minimum count is 1")
-    const allowedSites = {
-      'Odnoklassniki': /https?:\/\/ok.ru/,
-      'YouTube': 'https://www.youtube.com/watch?v='
-    };
-
-    const loadOKThumbnail = (source) =>
-      npm.axios(source)
-        .then(resp => resp.data)
-        .then(body => {
-          const searchingText = '<img src="'
-          let src = ''
-          for (let i = body.search(searchingText) + 10; body[i] != '"'; i++) {
-            src += body[i]
-          }
-          return src.replaceAll(';', '&');
-        })
-    const parseInternet = () => 
-      lib
-        .serpmaster
-        .search(query)
-        .then(response => response[0].content.results.organic)
-        .then(items => items.filter(item => {
-          for (const [host, url] of Object.entries(allowedSites)) {
-            if (
-              typeof url === 'string' && item.url.includes(url) ||
-              typeof url === 'object' && url.test(item.url)
-            ) {
-              item.host = host;
-              return true;
-            }
-          }
-        }))
-        .then(items => items.map(async item => {
-          const { title, desc, host } = item;
-          const updatedAt = new Date().toISOString()
-          switch (item.host) {
-            case "YouTube": {
-              const videoId = new URL(item.url).searchParams.get('v')
-              return {
-                stringId: host.concat("-", videoId),
-                title,
-                description: desc,
-                host,
-                source: `https://www.youtube.com/embed/${videoId}`,
-                thumbnail: `http://i3.ytimg.com/vi/${videoId}/hqdefault.jpg`,
-                updatedAt,
-              }
-            }
-            case "Odnoklassniki": {
-              const { url } = item
-              const videoId = url.slice(url.lastIndexOf('/') + 1, url.length);
-              const source = `https://ok.ru/videoembed/${videoId}`;
-              return {
-                stringId: host.concat("-", videoId),
-                title,
-                host,
-                description: desc,
-                source,
-                thumbnail: await loadOKThumbnail(source),
-                updatedAt,
-              };
-            }
-          }
-        }))
-        .then(promises => Promise.all(promises))
-        .then(async results => {
-          const scrappedVideoTypeId = await db.pg.col('VideoType', 'videoTypeId', { name: 'Scrapped' }).then(rows => rows[0])
-          for (const video of results) {
-            const rows = await db.pg.col('Video', 'videoId', { stringId: video.stringId })
-            if (rows.length) continue;
-            await db.pg.insert("Video", {
-              videoTypeId: scrappedVideoTypeId,
-              ...video,
-            })
-            db.pg.query(`UPDATE "Video" video SET "videoTokens" = to_tsvector(video.title) WHERE "stringId" = $1`, [video.stringId])
-          }
-          return results
-        })
     return db.pg.query(`
       SELECT 
       "title", 
@@ -100,11 +22,11 @@
       LIMIT $3
     `, [query, (page - 1) * count, count])
       .then(result => {
-        if(result.rows) {
-          parseInternet()
+        if(result.rows.length) {
+          domain.scrapnet(query)
           return result.rows
         }
-        return parseInternet()
+        return domain.scrapnet(query)
       })
   }
 })
--- db/structure.sql
+++ db/structure.sql
@@ -1,3 +1,4 @@
 ALTER TABLE "Video" ADD "videoTokens" TSVECTOR;
 CREATE INDEX "videoTokensIdx" ON "Video" USING GIN ("videoTokens");
 CREATE INDEX "stringIdIdx" ON "Video" ("stringId");
+CREATE INDEX "titleIdx" ON "Video" ("title");
