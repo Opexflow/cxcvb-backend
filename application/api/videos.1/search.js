44c2ba369eae9d955970b68e57811be8ba01407c
Saro Amirkhanyan <saroamirkhanyan@gmail.com>
2021-08-19 14:28:20 +0400

    added also linear search in search for better result

--- application/api/videos.1/search.js
+++ application/api/videos.1/search.js
@@ -9,28 +9,41 @@
     if (!validation.valid) return new Error(validation.errors)
     if (count > 30) return new Error("Maximum count is 30")
     if (count < 1) return new Error("Minimum count is 1")
-    return db.pg.query(`
+    const FTSDBResult = await db.pg.query(`
       SELECT 
-      "videoId",
-      "title", 
-      "host", 
-      "source", 
-      "thumbnail",
-      "description" FROM "Video" 
+        "videoId",
+        "title", 
+        "host", 
+        "source", 
+        "thumbnail",
+        "description" 
+      FROM "Video" 
       WHERE "videoTokens" @@ plainto_tsquery($1)
       ORDER BY ts_rank("videoTokens", plainto_tsquery($1)) DESC
       OFFSET $2 
       LIMIT $3
     `, [query, (page - 1) * count, count])
-      .then(results => {
-        if(results.rows.length) {
-          for (const video of results.rows) {
-            db.pg.query(`UPDATE "Video" SET score = score + 1 WHERE "videoId" = $1`, [video.videoId])
-          }
-          domain.scrapnet(query)
-          return results.rows
-        }
-        return domain.scrapnet(query)
-      })
+      .then(results => results.rows)
+    if(FTSDBResult.length) {
+      FTSDBResult.scrapnet()
+      return FTSDBResult
+    }
+    if(page != 1) return []
+    const LSDBResult = await db.pg.query(`
+      SELECT
+        "videoId",
+        "title", 
+        "host", 
+        "source", 
+        "thumbnail",
+        "description"
+      FROM "Video"
+      WHERE lower(title) LIKE lower($1)
+    `, [`%${query}%`]).then(results => results.rows)
+    if(LSDBResult.length) {
+      domain.scrapnet()
+      return LSDBResult
+    } 
+    return domain.scrapnet()
   }
 })
