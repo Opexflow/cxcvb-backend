3ca33d90b21cb6ff9e8fa4458c57e703ae3e2211
Saro Amirkhanyan <saroamirkhanyan@gmail.com>
2021-11-05 13:37:01 +0400

    Added getRandom Function

---
+++ application/api/videos.1/getRandom.js
@@ -0,0 +1,25 @@
+({
+  access: "public",
+  async method({ type: videoType = "all" }) {
+    let videoTypeId = -1;
+    if (videoType !== "all") {
+      const videoTypeIdResp = await db.pg.row("VideoType", ["videoTypeId"], { name: videoType })
+      if (!videoTypeIdResp) return new Error("Type not found")
+      videoTypeId = videoTypeIdResp.videoTypeId
+    }
+    console.log({ videoType, videoTypeId })
+    return db.pg.query(`
+      SELECT       
+        "videoId",
+        "title",
+        "host",
+        "source",
+        "thumbnail",
+        "description"
+      FROM "Video" 
+      WHERE ($1 = -1 OR "videoTypeId" = $1)
+      ORDER BY RANDOM() 
+      LIMIT 1
+    `, [videoTypeId]).then(result => result.rows[0])
+  }
+})
