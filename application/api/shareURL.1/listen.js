a1cf891c331d26570c053f1618ae7bf5e80e400d
Saro Amirkhanyan <saroamirkhanyan@gmail.com>
2021-11-06 19:03:53 +0400

    added listeners for connecting and disconnecting

--- application/api/shareURL.1/listen.js
+++ application/api/shareURL.1/listen.js
@@ -3,9 +3,15 @@
   method({ name }) {
     context.client.shareURLData = { name, id: Date.now() + metarhia.metautil.random(1, 10) }
     const network = domain.shareURL.getNetwork(context.client.ip)
+    for(const client of network) {
+      client.emit("shareURL/connected", context.client.shareURLData)
+    }
     network.add(context.client)
     context.client.on('close', () => {
       network.delete(context.client);
+      for(const client of network) {
+        client.emit("shareURL/disconnected", context.client.shareURLData)
+      }
     });
     return 'ok';
   }
