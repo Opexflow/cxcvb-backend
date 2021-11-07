7b65e8b0d6ae785a82af6d55330ec521a5ee232b
Saro Amirkhanyan <saroamirkhanyan@gmail.com>
2021-11-07 12:35:57 +0400

    updated Imperss and done some fixes

--- application/domain/kodikdb/start.js
+++ application/domain/kodikdb/start.js
@@ -1,7 +1,7 @@
 async () => {
   if (application.worker.id === 'W6') {
     const itemsCount = await db.pg.query(`SELECT COUNT('videoId') FROM "Video"`)
-      if(itemsCount.rows[0].count === "0") {
+      if(itemsCount.rows[0].count !== "0") {
         await domain.kodikdb.update({ intermediateTime: 100 })
       }
       application.scheduler.add({
--- application/domain/kodikdb/updateVideos.js
+++ application/domain/kodikdb/updateVideos.js
@@ -14,25 +14,27 @@
 
   parser.on('data', video => {
     videoQueue.add(async () => {
+      console.log(video)
       const tableItem = await db.pg.row('Video', ['videoId', 'remoteUpdatedAt'], { stringId: video.id || '' })
       const newItem = {
         stringId: video.id,
         videoTypeId,
         title: video.title,
-        description: video.material_data?.description,
+        description: video.material_data?.description || '',
         host: "Kodik",
         source: video.player_link,
-        thumbnail: video.material_data?.poster_url,
+        thumbnail: video.material_data?.poster_url || '',
         updatedAt,
         remoteUpdatedAt: video.updated_at,
         score: 0,
       }
       if(tableItem) {
         if(tableItem.remoteUpdatedAt !== video.updated_at) {
-           db.pg.update("Video", newItem, {
+          console.log({ newItem }) 
+          db.pg.update("Video", newItem, {
              stringId: video.id,
            })
-           await db.pg.query(`UPDATE "Video" video SET "videoTokens" = to_tsvector(video.title || ' ' || COALESCE($2, '')) WHERE "stringId" = $1`, [video.id, video.title_orig])
+          //  await db.pg.query(`UPDATE "Video" video SET "videoTokens" = to_tsvector(video.title || ' ' || COALESCE($2, '')) WHERE "stringId" = $1`, [video.id, video.title_orig])
         }
         return;
       }
